1b8a7429a62f47ba82b8df3ea8e1aee849f30bf4
Freezesploit <probuunny223@gmail.com>
2025-10-04 03:42:36 -0700

    Create flamecoderv1.lua

---
+++ flamecoderv1.lua
@@ -0,0 +1,7 @@
+-- FlameCoder Prison Life V1
+-- Hello my friend, don't take other people's scripts irresponsibly because it's not fair, so please don't do that, it could get my friend's sin^^ [From flame coder]
+-- Yeah guys skidding is bad so dont do it [from me]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Berliandakhensy/FlameCoder/refs/heads/main/obfuscated_script-1759030985020.lua.txt"))()
+
+-- NOT MADE BY FREEZESPLOIT (ME), CREDITS TO THE OWNER (KEN DEVELOPER)
