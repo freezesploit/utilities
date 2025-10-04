295070c9cc04b4e710e2856223c8f3cbb02c6941
Freezesploit <probuunny223@gmail.com>
2025-10-04 09:27:07 -0700

    Create D1NO13 script.lua

---
+++ D1NO13 script.lua
@@ -0,0 +1,5 @@
+--key is D1NO13
+-- Script not made by freezesploit, made by D1NO13 so creditz to him
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bozokongy-hash/mastxr/refs/heads/main/d1nohub.lua"))()
