631a330ed6f967f35d4fe720a8a31187a8598343
Freezesploit <probuunny223@gmail.com>
2025-10-07 11:51:55 -0700

    Create GGFFA V2 Script.lua

---
+++ GGFFA V2 Script.lua
@@ -0,0 +1,3 @@
+-- Not made by me bro im js showcasing
+-- Credits to who ever made this cah icl idek who made this
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gcklores-tech/GunGroundsFFA/refs/heads/main/main.lua",true))()
